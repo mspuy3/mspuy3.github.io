e439d3f74f34a70fbcb5823ff98385ca30043290
Michael Siemens Uy <89561283+mspuy3@users.noreply.github.com>
2021-11-16 20:08:22 +0800

    cleaned functions

--- simpleCalculatorWebApp/script.js
+++ simpleCalculatorWebApp/script.js
@@ -1,15 +1,14 @@
-let given1 = document.getElementById("given1").value;
-let given2 = document.getElementById("given2").value;
-let answer = document.getElementById("answer");
-let last = document.getElementById("last");
+
+
 
 function add() {
   let given1 = document.getElementById("given1").value;
   let given2 = document.getElementById("given2").value;
+
   if(given1 == "" || given2 ==""){
     answer.textContent = "Missing Given/s";
   } else {
-  let ans = Number(given1) + Number(given2);
+  let ans = ((Number(given1)*1000000000) + (Number(given2)*1000000000))/1000000000;
   answer.textContent = "The sum is: " + ans;
   last.innerHTML = "Last Calculation:</br>" + given1 + " + " + given2 + " = " + ans;
   }
@@ -18,10 +17,11 @@ function add() {
 function subtract() {
   let given1 = document.getElementById("given1").value;
   let given2 = document.getElementById("given2").value;
+
     if(given1 == "" || given2 ==""){
     answer.textContent = "Missing Given/s";
   } else {
-  let ans = Number(given1) - Number(given2);
+    let ans = ((Number(given1)*1000000000) - (Number(given2)*1000000000))/1000000000;
   answer.textContent = "The difference is: " + ans;
   last.innerHTML = "Last Calculation:</br>" + given1 + " - " + given2 + " = " + ans;
   }
@@ -30,10 +30,11 @@ function subtract() {
 function multiply() {
   let given1 = document.getElementById("given1").value;
   let given2 = document.getElementById("given2").value;
+
     if(given1 == "" || given2 ==""){
     answer.textContent = "Missing Given/s";
   } else {
-  let ans = Number(given1) * Number(given2);
+    let ans = ((Number(given1)*1000000000) * (Number(given2)*1000000000))/1000000000;
   answer.textContent = "The product is: " + ans;
   last.innerHTML = "Last Calculation:</br>" + given1 + " X " + given2 + " = " + ans;
   }
@@ -42,10 +43,11 @@ function multiply() {
 function divide() {
   let given1 = document.getElementById("given1").value;
   let given2 = document.getElementById("given2").value;
+
   if(given1 == "" || given2 ==""){
     answer.textContent = "Missing Given/s";
   } else {
-  let ans = Number(given1) / Number(given2);
+    let ans = ((Number(given1)*1000000000) / (Number(given2)*1000000000))/1000000000;
   answer.textContent = "The quotient is: " + ans;
   last.innerHTML = "Last Calculation:</br>" + given1 + " ÷ " + given2 + " = " + ans;
   }
@@ -54,10 +56,11 @@ function divide() {
 function remainder() {
   let given1 = document.getElementById("given1").value;
   let given2 = document.getElementById("given2").value;
+
   if(given1 == "" || given2 ==""){
     answer.textContent = "Missing Given/s";
   } else {
-  let ans = Number(given1) % Number(given2);
+    let ans = ((Number(given1)*1000000000) % (Number(given2)*1000000000))/1000000000;
   answer.textContent = "The remainder is: " + ans;
   last.innerHTML = "Last Calculation:</br>" + given1 + " % " + given2 + " = " + ans;
   }
@@ -68,3 +71,15 @@ function clearOut() {
   document.getElementById("given2").value = "";
   answer.textContent = "Answer Appears Here";
 }
+
+
+
+
+document.querySelector("#add-btn").addEventListener("click", add);
+document.querySelector("#subtract-btn").addEventListener("click", subtract);
+document.querySelector("#multiply-btn").addEventListener("click", multiply);
+document.querySelector("#divide-btn").addEventListener("click", divide);
+document.querySelector("#remainder-btn").addEventListener("click", remainder);
+
+
+document.querySelector("#clear-btn").addEventListener("click", clearOut);
